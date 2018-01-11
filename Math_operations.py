ea37e10e3e99b89260b03ebdee93789d9f6c0b38
Prathiksha-Hegde <35320251+Prathiksha-Hegde@users.noreply.github.com>
2018-01-10 20:03:54 -0700

    Add files via upload

---
+++ Math_operations.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 19:30:05 2018
+
+@author: Hari Shayam
+"""
+
+a = int(input("Enter a number\n"))
+b = int(input("Enter second number\n"))
+c=a+b
+print("Sum is {0}".format(c))
+d=a-b
+print("Difference is {0}".format(d))
+e=a*b
+print("Product is {0}".format(e))
